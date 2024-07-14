32e28dcf41bb9770128ffc368427f97f5ec091c2
Cesar Villanueva Jr <codeengie@protonmail.com>
2024-07-14 16:45:35 -0700

    No User Reviews
    Updated stats component to display a "No user reviews" when the count
    is set to 0.

--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -3,15 +3,23 @@ import './Stats.scss';
 const Stats = ({reviews, ...props}) => {
     let cssClasses = `stats`;
     const prettifyCount = `(${reviews['count'].toLocaleString('en-US')})`;
+    let content = 'No user reviews';
 
     if (props.className) {
         cssClasses += ` ${props.className}`;
     }
 
-    return (
-        <div className={cssClasses}>
+
+    if (reviews.count !== 0) {
+        content = <>
             <span className="stats__review">{reviews.summary}</span>
             <span className="stats__count">{prettifyCount}</span>
+        </>
+    }
+
+    return (
+        <div className={cssClasses}>
+            {content}
         </div>
     )
 }
