ea0cbccf6b71a4270f1ce99f843c540643a8a0e9
Cesar Villanueva Jr <codeengie@protonmail.com>
2024-07-13 22:48:18 -0700

    Dynamic Media
    I removed hard coded asset importing and made changes to the component
    to set the image and video based on the game data.
    
    Along with the above change I also moved my assets to the public folder
    to keep things simple. Typically, I would upload them to a S3 bucket or
    create a component to dynamically load the images.

--- src/components/capsule-card/CapsuleCard.jsx
+++ src/components/capsule-card/CapsuleCard.jsx
@@ -2,27 +2,24 @@ import './CapsuleCard.scss';
 import Discount from "../discount/Discount.jsx";
 import Stats from "../stats/Stats.jsx";
 import Tags from "../tags/Tags.jsx";
-// Import assets (temporary solution)
-import CapsulePoster from '../../assets/images/hogwarts-legacy.jpg'
-import CapsuleVideo from '../../assets/videos/hogwarts-legacy.webm'
 
 const CapsuleCard = (props) => {
     return (
         <div className="capsule-card">
             <a className="capsule-card__link" href="#">
-                <img className="capsule-card__poster" src={CapsulePoster} alt="Main capsule card image"/>
+                <img className="capsule-card__poster" src={`/images/${props.data.media.poster}`} alt="Main capsule card image"/>
             </a>
             <div className="capsule-card__content">
                 {/* Display a screenshot or video */}
                 <div className="capsule-card__media">
                     <video className="capsule-card__video" autoPlay loop>
-                        <source src={CapsuleVideo} type="video/webm"/>
+                        <source src={`/videos/${props.data.media.video}`} type="video/webm"/>
                     </video>
                 </div>
                 <div className="capsule-card__info">
                     <h1 className="capsule-card__title">{props.data.title}</h1>
-                    <Stats className="capsule-card__stats"/>
-                    <Tags className="capsule-card__tags"/>
+                    <Stats className="capsule-card__stats" reviews={props.data.reviews}/>
+                    <Tags className="capsule-card__tags" tags={props.data.tags}/>
                     <button className="button capsule-card__button">Add to Cart</button>
                 </div>
             </div>
