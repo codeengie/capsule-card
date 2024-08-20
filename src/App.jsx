5e3e9168168fb91c0edf63844c5e486a4ade8277
Cesar Villanueva Jr <codeengie@protonmail.com>
2024-08-19 17:45:54 -0700

    Dummy Data
    Moved dummy data from App.jsx to local files. Created a new directory
    and two files for the dummy data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,7 @@
 import './App.scss'
-import CapsuleCard from "./components/capsule-card/CapsuleCard.jsx";
-
-const games = [
-    {
-        title: "Hogwarts Legacy",
-        price: 59.99,
-        discount: 60,
-        tags: ['Magic', 'Fantasy', 'Open World', 'Singleplayer', 'Adventure'],
-        reviews: {
-            count: 195189,
-            summary: "Very Positive"
-        },
-        media: {
-            poster: "hogwarts-legacy-374x448.webp",
-            video: "hogwarts-legacy-630x354.webm"
-        }
-    },
-    {
-        title: "The Last Of Us Part I",
-        price: 59.99,
-        discount: 0,
-        tags: ['Shooter', 'Story Rich', 'Post-apocalyptic', 'Horror'],
-        reviews: {
-            count: 31084,
-            summary: "Mostly Positive"
-        },
-        media: {
-            poster: "the-last-of-us-374x448.webp",
-            video: "the-last-of-us-630x354.webm"
-        }
-    },
-    {
-        title: "Marvel vs. Capcom Fighting Collection: Arcade Classics",
-        price: 39.99,
-        discount: 12,
-        tags: ['Action', '2D Fighter', 'Multiplayer', 'Singleplayer'],
-        reviews: {
-            count: 0,
-            summary: ""
-        },
-        media: {
-            poster: "mvc-arcade-classics-374x448.webp",
-            video: "mvc-arcade-classics-630x354.webm"
-        }
-    },
-    {
-        title: "Star Wars Jedi: Survivor",
-        price: 69.99,
-        discount: 20,
-        tags: ['Action', 'Adventure', 'Souls-like', 'Singleplayer'],
-        reviews: {
-            count: 49684,
-            summary: "Mixed"
-        },
-        media: {
-            poster: "star-wars-jedi-survivor-374x448.webp",
-            video: "star-wars-jedi-survivor-630x354.webm"
-        }
-    }
-];
+import CapsuleCard from './components/capsule-card/CapsuleCard.jsx';
+import games from './data/games.jsx';
+import user from './data/user.jsx';
 
 function App() {
     let content = null;
@@ -69,7 +12,9 @@ function App() {
 
     return (
         <>
-            {content}
+            <div className="wrap">
+                {content}
+            </div>
         </>
     )
 }
---
+++ src/data/user.jsx
@@ -0,0 +1,15 @@
+const user = {
+    uid: "nzKfAOT7O1",
+    uname: "madeupuser",
+    email: "madeupuser@nothing.com",
+    owned: [
+        { gid: "gHVy33Begs" },
+        { gid: "L2Ucz6viT8" },
+        { gid: "2PgAOlzgVO" },
+    ],
+    wishlist: [
+        { gid: "kfqgc65YG9" }
+    ]
+};
+
+export default user;
